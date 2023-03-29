c6d78c19496080997973d958beb63bf5b5f735bf
cemilebalci <cemile148.cb@gmail.com>
2023-03-29 10:48:19 +0300

    first commit

---
+++ randomtopla.py
@@ -0,0 +1,11 @@
+import random
+
+# iki rasgele sayı üretme #
+sayi1 = random.randint(1,100)
+sayi2 = random.randint(1,100)
+
+# iki sayısının toplamı #
+toplam = sayi1 + sayi2
+
+print ("ilk sayı:",sayi1, "ikinci sayı:", sayi2 )
+print ("toplam:", toplam)
